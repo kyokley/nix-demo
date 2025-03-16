f090104cada5646a6967bcaacc17d18cbb2797b5
Kevin Yokley <kyokley@mars>
2025-03-16 18:01:46 -0500

    Initial commit

---
+++ devenv.nix
@@ -0,0 +1,54 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/basics/
+  env.GREET = "Nix Demo";
+
+  # https://devenv.sh/packages/
+  packages = [];
+
+  # https://devenv.sh/languages/
+  languages = {
+    python = {
+      enable = true;
+      version = "3.12";
+      uv.enable = true;
+    };
+  };
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  scripts.hello.exec = ''
+    echo Welcome to $GREET
+    echo
+  '';
+
+  enterShell = ''
+    hello
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  # enterTest = ''
+  #   echo "Running tests"
+  #   git --version | grep --color=auto "${pkgs.git.version}"
+  # '';
+
+  # https://devenv.sh/git-hooks/
+  # git-hooks.hooks = {
+  #   markdownlint.enable = true;
+  #   html-tidy.enable = true;
+  # };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
---
+++ docs/index.md
@@ -0,0 +1,1 @@
+# Nix The World
